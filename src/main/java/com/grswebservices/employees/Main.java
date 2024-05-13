70b1e61e1b5581a19fe4f4a200141adfa061829e
grs-se <george_ricesmith@hotmail.com>
2024-05-13 12:55:27 +0100

    Implement other employee classes

--- src/main/java/com/grswebservices/employees/Main.java
+++ src/main/java/com/grswebservices/employees/Main.java
@@ -7,39 +7,27 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         String peopleText = """
-    Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-    Flinstone, Fred, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
-    Flinstone, Fred, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
-    Flinstone, Fred, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
-    Flinstone, Fred, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
-    Rubble, Barney, 2/2/1905, Manager, {orgSize=300,dr=10}
-    Rubble, Barney, 2/2/1905, Manager, {orgSize=100,dr=4}
-    Rubble, Barney, 2/2/1905, Manager, {orgSize=200,dr=2}
-    Rubble, Barney, 2/2/1905, Manager, {orgSize=500,dr=8}
-    Rubble, Barney, 2/2/1905, Manager, {orgSize=175,dr=20}
-    Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=3}
-    Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=4}
-    Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=5}
-    Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=6}
-    Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=9}
-    Rubble, Betty, 4/4/1915, CEO, {avgStockPrice=300}
-    """;
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
+            Rubble, Barney, 2/2/1905, Manager, {orgSize=300,dr=10}
+            Rubble, Barney, 2/2/1905, Manager, {orgSize=100,dr=4}
+            Rubble, Barney, 2/2/1905, Manager, {orgSize=200,dr=2}
+            Rubble, Barney, 2/2/1905, Manager, {orgSize=500,dr=8}
+            Rubble, Barney, 2/2/1905, Manager, {orgSize=175,dr=20}
+            Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=3}
+            Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=4}
+            Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=5}
+            Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=6}
+            Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=9}
+            Rubble, Betty, 4/4/1915, CEO, {avgStockPrice=300}
+            """;
 
         String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?\\n";
         Pattern peoplePat = Pattern.compile(peopleRegex);
         Matcher peopleMat = peoplePat.matcher(peopleText);
-
-        String progRegex = "\\w+=(?<locpd>\\w+),\\w+=(?<yoe>\\w),\\w+=(?<iq>\\w)";
-        Pattern coderPat = Pattern.compile(progRegex);
-
-        String mgrRegex = "\\w+=(?<orgSize>\\w+),\\w+=(?<dr>\\w+)";
-        Pattern mgrPat = Pattern.compile(mgrRegex);
-
-        String analystRegex = "\\w+=(?<projectCount>\\w+)";
-        Pattern analystPat = Pattern.compile(analystRegex);
-
-        String ceoRegex = "\\w+=(?<avgStockPrice>\\w+)";
-        Pattern ceoPat = Pattern.compile(ceoRegex);
 
         int totalSalaries = 0;
         // peopleMat.find() returns a boolean
@@ -51,64 +39,31 @@ public class Main {
                     yield programmer.getSalary();
                 }
                 case "Manager" -> {
-                    String details = peopleMat.group("details");
-                    Matcher mgrMat = mgrPat.matcher(details);
-                    int salary = 0;
-                    if (mgrMat.find()) {
-                        int orgSize = Integer.parseInt(mgrMat.group("orgSize"));
-                        int directReports = Integer.parseInt(mgrMat.group("dr"));
-                        salary = 3500 + orgSize * directReports;
-                    } else {
-                        salary = 3500;
-                    }
-                    String lastName = peopleMat.group("lastName");
-                    String firstName = peopleMat.group("firstName");
-                    System.out.printf("%s: %s: %s%n", lastName, firstName, NumberFormat.getCurrencyInstance().format(salary));
-                    yield salary;
+                    Manager manager = new Manager(peopleMat.group());
+                    System.out.println(manager.toString());
+                    yield manager.getSalary();
                 }
                 case "Analyst" -> {
-                    String details = peopleMat.group("details");
-                    Matcher analystMat = analystPat.matcher(details);
-                    int salary = 0;
-                    if (analystMat.find()) {
-                        int projectCount = Integer.parseInt(analystMat.group("projectCount"));
-                        salary = 2500 + projectCount * 2;
-                    } else {
-                        salary = 2500;
-                    }
-                    String lastName = peopleMat.group("lastName");
-                    String firstName = peopleMat.group("firstName");
-                    System.out.printf("%s: %s: %s%n", lastName, firstName, NumberFormat.getCurrencyInstance().format(salary));
-                    yield salary;
+                    Analyst analyst = new Analyst(peopleMat.group());
+                    System.out.println(analyst.toString());
+                    yield analyst.getSalary();
                 }
                 case "CEO" -> {
-                    String details = peopleMat.group("details");
-                    Matcher ceoMat = ceoPat.matcher(details);
-                    int salary = 0;
-                    if (ceoMat.find()) {
-                        int avgStockPrice = Integer.parseInt(ceoMat.group("avgStockPrice"));
-                        salary = 5000 * avgStockPrice;
-                    } else {
-                        salary = 5000;
-                    }
-                    String lastName = peopleMat.group("lastName");
-                    String firstName = peopleMat.group("firstName");
-                    System.out.printf("%s: %s: %s%n", lastName, firstName, NumberFormat.getCurrencyInstance().format(salary));
-                    yield salary;
+                    CEO ceo = new CEO(peopleMat.group());
+                    System.out.println(ceo.toString());
+                    yield ceo.getSalary();
                 }
                 default -> 0;
             };
-//            System.out.printf("%s %s %s %s%n", peopleMat.group("firstName"), peopleMat.group("lastName"), peopleMat.group("dob"), peopleMat.group("role"));
         }
         NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
         System.out.printf("The total payout should be %s%n", currencyInstance.format(totalSalaries));
-        ;
     }
 }
 
 
 /*
-/// BAD PROCEDURAL VERSION
+/// BAD PROCEDURAL IMPLEMENTATION
 package com.grswebservices.employees;
 
 import java.text.NumberFormat;
--- src/main/java/com/grswebservices/employees/Programmer.java
+++ src/main/java/com/grswebservices/employees/Programmer.java
@@ -19,8 +19,7 @@ public class Programmer {
     private final String progRegex = "\\w+=(?<locpd>\\w+),\\w+=(?<yoe>\\w),\\w+=(?<iq>\\w)";
     private final Pattern progPat = Pattern.compile(progRegex);
     private final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
-
-    DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+    private final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
 
     public Programmer(String personText) {
         Matcher peopleMat = peoplePat.matcher(personText);
